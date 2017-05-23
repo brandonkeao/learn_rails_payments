8c823654cfa59b573e0f782fb6c3f439f9a7e456
brandonkeao <brandon.keao@gmail.com>
2017-05-22 22:51:48 -0600

    Added javascript to home view for single buy button for Stripe using bootstrap. Also loaded stripe keys into env variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  ENV['PUBLISHABLE_KEY'] = 'pk_test_XnyQaNxc8ez6glt4AoAcmjAP'
+  ENV['SECRET_KEY'] = 'sk_test_nHtp0ekP28rXrwG1y09oHQuM'
+
 end
